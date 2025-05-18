a97857b061e921c817c171960dfb11f6a81e9dd7
Arnav Sutraway <asutraway@gmail.com>
2025-05-17 21:14:19 -0400

    Created an about html file and created a path for it in flask backend

--- frontend/app.py
+++ frontend/app.py
@@ -8,5 +8,9 @@ app = Flask(__name__, static_folder='static', template_folder='templates')
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
