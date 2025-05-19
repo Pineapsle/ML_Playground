1ebd2b7dacdd068a1b2d60d03bc4f6e7650712ac
Arnav Sutraway <asutraway@gmail.com>
2025-05-19 18:36:25 -0400

    Made styles for all html files and created paths for all models to their respective html pages. Updated app.py to handle it all

--- frontend/app.py
+++ frontend/app.py
@@ -12,5 +12,23 @@ def index():
 def about():
     return render_template('about.html')
 
+# Models Routes
+
+@app.route('/lin_reg', methods=['GET', 'POST'])
+def lin_reg():
+    return render_template('lin_reg.html')
+
+@app.route('/log_reg', methods=['GET', 'POST'])
+def log_reg():
+    return render_template('log_reg.html')
+
+@app.route('/dec_tree', methods=['GET', 'POST'])
+def dec_tree():
+    return render_template('dec_tree.html')
+
+@app.route('/neural', methods=['GET', 'POST'])
+def neural():
+    return render_template('neural.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
