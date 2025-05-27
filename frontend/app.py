5a80990820b9d96a13ae0f804600f1581b6570cb
Arnav Sutraway <asutraway@gmail.com>
2025-05-27 02:05:09 -0400

    Styled and added chart and path for images. Also made html files and css files better with better design and made all the designs the same

--- frontend/app.py
+++ frontend/app.py
@@ -9,18 +9,43 @@ def index():
     return render_template('index.html')
 
 @app.route('/about')
-def about():
+def about():    
     return render_template('about.html')
 
 # Models Routes
 
 @app.route('/lin_reg', methods=['GET', 'POST'])
 def lin_reg():
-    return render_template('lin_reg.html')
+    # model results
+    mse = 1595610795.60622
+    rmse = 39945.09727
+    r2 = 0.79823
+    r = 0.89343
+    return render_template(
+        'lin_reg.html',
+        mse=mse,
+        rmse=rmse,
+        r2=r2,
+        r=r
+        )
 
 @app.route('/log_reg', methods=['GET', 'POST'])
 def log_reg():
-    return render_template('log_reg.html')
+    # model results
+    confusion_matrix = [[92, 9,
+                        7, 116]]
+    precision = 0.92800
+    recall = 0.94309
+    f1_score = 0.93548
+    accuracy = 0.92857
+    return render_template(
+        'log_reg.html',
+        confusion_matrix=confusion_matrix,
+        precision=precision,
+        recall=recall,
+        f1_score=f1_score,
+        accuracy=accuracy
+        )
 
 @app.route('/dec_tree', methods=['GET', 'POST'])
 def dec_tree():
