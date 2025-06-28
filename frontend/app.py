25e4dcd27c02ab81c54bd95705baf0827549b87c
Arnav Sutraway <asutraway@gmail.com>
2025-06-28 15:38:40 -0400

    completed data inputs in html files for the models. Also added gifs

--- frontend/app.py
+++ frontend/app.py
@@ -49,11 +49,53 @@ def log_reg():
 
 @app.route('/dec_tree', methods=['GET', 'POST'])
 def dec_tree():
-    return render_template('dec_tree.html')
+    # Model Results
+    confusion_matrix = [[20705, 1919,
+                        25215, 2337]]
+    precision = 0.54911
+    recall = 0.08482
+    f1_score = 0.14694
+    accuracy = 0.45922
+    # Iterations
+    iterations = {
+        0 : 0.6930,
+        100 : 0.6928,
+        200 : 0.6926,
+        300 : 0.6923,
+        400 : 0.6919,
+        500 : 0.6913,
+        600 : 0.6902,
+        700 : 0.6885,
+        800 : 0.6857,
+        900 : 0.6811 
+    } 
+    return render_template(
+        'dec_tree.html',
+        confusion_matrix=confusion_matrix,
+        precision=precision,
+        recall=recall,
+        f1_score=f1_score,
+        accuracy=accuracy,
+        iterations=iterations
+        )
 
 @app.route('/neural', methods=['GET', 'POST'])
 def neural():
-    return render_template('neural.html')
+    # Model Results
+    confusion_matrix = [[20705, 1919,
+                        25215, 2337]]
+    precision = 0.54911
+    recall = 0.08482
+    f1_score = 0.14694
+    accuracy = 0.45922
+    return render_template(
+        'neural.html',
+        confusion_matrix=confusion_matrix,
+        precision=precision,
+        recall=recall,
+        f1_score=f1_score,
+        accuracy=accuracy
+        )
 
 if __name__ == '__main__':
     app.run(debug=True)
